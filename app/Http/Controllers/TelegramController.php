16e4048dc3a9009b99a0edc1b15f5d405d76be8e
JordiWP <jgtorcal@gmail.com>
2021-02-13 15:45:07 +0100

    up

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -101,16 +101,16 @@ class TelegramController extends Controller
 
         $activity = Telegram::getUpdates();
 
-        dd($activity);
+        //dd($activity);
 
-        foreach ($activity as $item){
+        foreach ($activity['items'] as $item){
             Updates::create(array(
-                'update_id'     => $item['items']['update_id'],
-                'message_id'    => $item['items']['message']['message_id'],
-                'from_id'       => $item['items']['message']['from']['id'],
-                'from_username' => $item['items']['message']['from']['username'],
-                'chat_id'       => $item['items']['message']['chat']['id'],
-                'text'          => $item['items']['message']['text']
+                'update_id'     => $item['update_id'],
+                'message_id'    => $item['message']['message_id'],
+                'from_id'       => $item['message']['from']['id'],
+                'from_username' => $item['message']['from']['username'],
+                'chat_id'       => $item['message']['chat']['id'],
+                'text'          => $item['message']['text']
             ));
         }
 
