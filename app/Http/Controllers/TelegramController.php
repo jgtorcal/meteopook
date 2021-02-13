eef9b462fbc640fd06765b292524f7cebc6b749e
JordiWP <jgtorcal@gmail.com>
2021-02-13 03:04:24 +0100

    entrada

---
+++ app/Http/Controllers/TelegramController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+use DB;
+
+// -1001291111565 Pay to Cry
+
+class TelegramController extends Controller
+{
+    public function index(){
+
+        $activity = Telegram::getUpdates();
+        //$col = collect($activity);
+
+        //dd($activity);
+
+        return $activity;
+
+    }
+
+    public function enviar(){
+
+        Telegram::sendMessage([
+            'chat_id' => '-1001291111565',
+            'text' => 'Te doy la mano'
+        ]);
+        return;
+
+    }
+
+
+    public function enviartest(){
+
+        Telegram::sendMessage([
+            'chat_id' => '170018514',
+            'text' => 'Te doy la mano'
+        ]);
+        return;
+
+    }
+    
+
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TelegramController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('telegramupdates', [App\Http\Controllers\TelegramController::class, 'index'])->name('telegram.index');
+
+Route::get('enviarmensaje', [App\Http\Controllers\TelegramController::class, 'enviar'])->name('telegram.enviar');
+
+Route::get('enviartest', [App\Http\Controllers\TelegramController::class, 'enviartest'])->name('telegram.enviartest');
