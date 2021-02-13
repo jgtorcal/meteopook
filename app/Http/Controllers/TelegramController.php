e8f851f71f738240a0fe698f6a83a902d4af4c68
JordiWP <jgtorcal@gmail.com>
2021-02-13 03:48:12 +0100

    updates telegram

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -13,12 +13,18 @@ class TelegramController extends Controller
 {
     public function index(){
 
-        $activity = Telegram::getUpdates();
-        //$col = collect($activity);
+        // $activity = Telegram::getUpdates();
+        // //$col = collect($activity);
 
-        //dd($activity);
+        // //dd($activity);
 
-        return $activity;
+        // return $activity;
+
+        Telegram::sendMessage([
+            'chat_id' => '170018514',
+            'text' => 'Te doy la mano'
+        ]);
+        return;
 
     }
 
