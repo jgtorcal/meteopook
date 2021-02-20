58759d3b02db89751f14b592d62679521df84df6
JordiWP <jgtorcal@gmail.com>
2021-02-20 01:19:26 +0100

    cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('inspire')->hourly();
         $schedule->command('backup:clean')->daily()->at('01:00');
-        $schedule->command('backup:run')->daily()->at('00:17');
+        $schedule->command('backup:run')->daily()->at('01:10');
     }
 
     /**
