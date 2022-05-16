cee27c1f85fe42ad881316b7712a1654ea83efc6
JordiWP <jgtorcal@gmail.com>
2022-05-17 00:39:54 +0200

    test

--- resources/views/lloro/index.blade.php
+++ resources/views/lloro/index.blade.php
@@ -11,7 +11,7 @@
                 <table class=" table table-hover">
                     <thead>
                         <tr>
-                            <th>Llorica</th>
+                            <th>Llorica!</th>
                             <th>Lloro</th>
                             <th>Fecha</th>
                         </tr>
