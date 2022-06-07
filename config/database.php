259bc26948638a3e4d915195a3a5a3ccceea6c64
Jordi Garcia <jgtorcal@gmail.com>
2022-06-08 01:44:43 +0200

    bk

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'dump' => [
-                'dump_binary_path' => env('MYSQLDUMP'), // only the path, so without `mysqldump` or `pg_dump`
+                //'dump_binary_path' => env('MYSQLDUMP'), // only the path, so without `mysqldump` or `pg_dump`
                 'use_single_transaction',
                 'timeout' => 60 * 5, // 5 minute timeout
              ],
